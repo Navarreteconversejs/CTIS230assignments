22a2cfe5fd9e0724bff5b90a03e0ee6d0b9a2ea0
Navarreteconversejs <navarreteconversejs@guilford.edu>
2021-04-27 08:10:50 -0400

    Changed id on navarrete and added two buttons to both navarrete and js

--- testscript.js
+++ testscript.js
@@ -1,9 +1,10 @@
 myitem = document.getElementById("firsttest");
-
+//The first line asks the webpage to get the element by the name of the ID. In this case the name was "firsttest". Javascript will then know this element as myItem.
     myitem.addEventListener("click", onClick);
     myitem.addEventListener("mouseout", onMouseOut);
-
+//The second line after myitem. gives the text the action that it needs to be prepared to act on. In this case, the action occurs after the item is clicked. The "action" is called a function.
     function onClick() {  
+        //The third line is the function that is done after the item is clicked. In this case the function is turning the text red.
         myitem.style.color= "red";  
         myitem.style.backgroundColor= "black";
     }
@@ -11,6 +12,27 @@ function onMouseOut() {
     myitem.style.color= "teal";
     myitem.style.backgroundColor= "white";
 }
-//The first line asks the webpage to get the element by the name of the ID. In this case the name was "firsttest". Javascript will then know this element as myItem.
-//The second line after myitem. gives the text the action that it needs to be prepared to act on. In this case, the action occurs after the item is clicked. The "action" is called a function.
-//The third line is the function that is done after the item is clicked. In this case the function is turning the text red.
+
+
+
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
+
+    thebutton.addEventListener("click", onButtonClick);
+
+    function onButtonClick() {  
+        otheritem.style.color = "red";  
+    }
+
+thesecondbutton = document.getElementById("thesecondbutton");  
+secondtest = document.getElementById("secondtest");
+
+thesecondbutton.addEventListener("click", onClick);
+    function onClick() {  
+        secondtest.style.backgroundColor = "black";  
+    }
+
+
+        
+ 
+ 
