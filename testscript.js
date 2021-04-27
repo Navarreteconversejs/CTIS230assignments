0fde445aa145e168053358995e87e29acc4c37a5
Navarreteconversejs <navarreteconversejs@guilford.edu>
2021-04-27 04:54:00 -0400

    update to testscript.js
    Added notes on what each line of code does. Added a mouseout function that returns the paragraph back to how it looked before it was clicked.

--- testscript.js
+++ testscript.js
@@ -1,7 +1,16 @@
 myitem = document.getElementById("firsttest");
 
     myitem.addEventListener("click", onClick);
+    myitem.addEventListener("mouseout", onMouseOut);
 
     function onClick() {  
         myitem.style.color= "red";  
+        myitem.style.backgroundColor= "black"
     }
+function onMouseOut() {
+    myitem.style.color= "teal"
+    myitem.style.backgroundColor= "white"
+}
+//The first line asks the webpage to get the element by the name of the ID. In this case the name was "firsttest". Javascript will then know this element as myItem.
+//The second line after myitem. gives the text the action that it needs to be prepared to act on. In this case, the action occurs after the item is clicked. The "action" is called a function.
+//The third line is the function that is done after the item is clicked. In this case the function is turning the text red.
